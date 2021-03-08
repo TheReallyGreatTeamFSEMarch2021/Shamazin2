e73610ebf424986788d8329073c5e201b165dda9
Fei Yang <fyang@C02CMKHKML7H.local>
2021-03-08 12:27:30 -0500

    first commit

---
+++ showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/ShowItemPageApplication.java
@@ -0,0 +1,13 @@
+package com.talentpath.shamazin.showItemPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowItemPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowItemPageApplication.class, args);
+	}
+
+}
---
+++ showItemPageBE/src/test/java/com/talentpath/shamazin/showItemPage/ShowItemPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentpath.shamazin.showItemPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowItemPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
