dada260ba8e1f6d32db914de6ac1516778021bb9
neveland98 <54957992+neveland98@users.noreply.github.com>
2021-03-08 14:39:35 -0600

    added jpa integration to project using Info class as an example

--- showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/Models/Info.java
+++ showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/Models/Info.java
@@ -1,4 +1,54 @@
 package com.talentpath.shamazin.showItemPage.Models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="Info")
 public class Info {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private String value;
+
+    public Info() {
+    }
+
+    public Info(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Info info = (Info) o;
+
+        if (!id.equals(info.id)) return false;
+        return value.equals(info.value);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + value.hashCode();
+        return result;
+    }
 }
