0a5fabe7071f9e3578e9c987ebd2e711635d05f0
Fei Yang <fyang@C02CMKHKML7H.local>
2021-03-09 14:45:09 -0500

    added ItemModels and productPhotoModel

---
+++ showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/models/ProductPhoto.java
@@ -0,0 +1,49 @@
+package com.talentpath.shamazin.showItemPage.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.Cascade;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+
+@Entity
+@Table(name="ProductPhotos")
+public class ProductPhoto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String photoURL;
+
+    @JsonIgnore
+    @ManyToOne(fetch=FetchType.LAZY)
+    @NotBlank
+    private Item item;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPhotoURL() {
+        return photoURL;
+    }
+
+    public void setPhotoURL(String photoURL) {
+        this.photoURL = photoURL;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+}
+
--- showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/models/Question.java
+++ showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/models/Question.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Table(name="questions")
+@Table(name="Questions")
 public class Question {
 
     @Id
