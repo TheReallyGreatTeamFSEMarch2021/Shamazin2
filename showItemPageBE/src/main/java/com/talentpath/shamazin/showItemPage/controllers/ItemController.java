40bdbb1141a75cd3f013c25f46b4893ce921d1c0
neveland98 <54957992+neveland98@users.noreply.github.com>
2021-03-09 15:32:52 -0600

    began setting up rest controller

--- showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/controllers/ItemController.java
+++ showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/controllers/ItemController.java
@@ -1,4 +1,16 @@
 package com.talentpath.shamazin.showItemPage.controllers;
 
+import com.talentpath.shamazin.showItemPage.services.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
 public class ItemController {
+    @Autowired
+    ItemService service;
+
 }
