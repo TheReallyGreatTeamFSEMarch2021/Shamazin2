84e4c2030906ae969eef00cb5140b0455e2dd8a4
jaitken <31525687+jaitken@users.noreply.github.com>
2021-03-10 14:54:46 -0500

    added getByFamilyId path for reviews (#22)

--- showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/controllers/ReviewController.java
+++ showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/controllers/ReviewController.java
@@ -3,10 +3,7 @@ package com.talentpath.shamazin.showItemPage.controllers;
 import com.talentpath.shamazin.showItemPage.models.Review;
 import com.talentpath.shamazin.showItemPage.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,5 +17,10 @@ public class ReviewController {
     @GetMapping("/getAll")
     public List<Review> getAllReviews() {
         return service.getAllReviews();
+    }
+
+    @GetMapping("getByItemFamily/{itemFamily}")
+    public List<Review> getByItemFamily(@PathVariable Integer itemFamily){
+        return service.getByItemFamily(itemFamily);
     }
 }
