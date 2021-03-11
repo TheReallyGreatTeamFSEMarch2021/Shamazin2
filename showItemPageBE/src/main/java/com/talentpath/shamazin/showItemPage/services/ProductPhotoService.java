71bda6e2e2b50a71c73a3c2e5d7dc29764b74b44
Fei Yang <feidyang@gmail.com>
2021-03-11 10:14:26 -0500

    changed method names in controller (#26)
    Co-authored-by: Fei Yang <fyang@C02CMKHKML7H.local>
    Co-authored-by: Nathaniel Eveland <54957992+neveland98@users.noreply.github.com>

---
+++ showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/services/ProductPhotoService.java
@@ -0,0 +1,40 @@
+package com.talentpath.shamazin.showItemPage.services;
+
+import com.talentpath.shamazin.showItemPage.daos.ProductPhotoRepository;
+import com.talentpath.shamazin.showItemPage.models.Item;
+import com.talentpath.shamazin.showItemPage.models.ProductPhoto;
+import org.hibernate.procedure.ProcedureOutputs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+@Component
+public class ProductPhotoService {
+    @Autowired
+    ProductPhotoRepository productPhotoRepo;
+
+    public List<ProductPhoto> getProductPhotosByItsItem(Item item){
+        Optional<List<ProductPhoto>> productPhotos = productPhotoRepo.findByItem(item);
+        try{
+            productPhotos.get().get(0).getPhotoURL();
+        }catch(NoSuchElementException ex){
+            System.out.println("No product photos associated with that item. " + ex);
+        }
+
+        return productPhotos.get();
+
+    }
+
+    public ProductPhoto getProductPhotoByID(Integer productID) {
+        Optional<ProductPhoto> productPhoto = productPhotoRepo.findById(productID);
+        try{
+            productPhoto.get();
+        }catch(NoSuchElementException ex){
+            System.out.println(("No product photo associated with that product ID. " + ex));
+        }
+        return productPhoto.get();
+    }
+}
