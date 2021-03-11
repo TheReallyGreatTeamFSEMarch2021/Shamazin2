d64605887656bb7a2e5ef278ee4e3e4f59508ec6
neveland98 <54957992+neveland98@users.noreply.github.com>
2021-03-11 13:08:33 -0600

    added controller endpoint for getting all item families

--- showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/controllers/ItemFamilyController.java
+++ showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/controllers/ItemFamilyController.java
@@ -1,15 +1,26 @@
 package com.talentpath.shamazin.showItemPage.controllers;
 
 
+import com.talentpath.shamazin.showItemPage.models.ItemFamily;
+import com.talentpath.shamazin.showItemPage.services.ItemFamilyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api/itemFamily")
 public class ItemFamilyController {
-    //todo: @Autowired ItemFamilyService when pull request gets approved
+    @Autowired
+    ItemFamilyService service;
+
+    @GetMapping("/getAll")
+    public List<ItemFamily> getAllItemFamilies() {
+        return service.getAllItemFamilies();
+    }
 
 }
