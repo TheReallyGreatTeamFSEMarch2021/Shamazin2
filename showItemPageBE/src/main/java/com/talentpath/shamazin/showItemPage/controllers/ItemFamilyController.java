eb58d92904447c6d060f9c69bd4d0a9935198e47
neveland98 <54957992+neveland98@users.noreply.github.com>
2021-03-11 12:44:46 -0600

    created controller for itemFamily

---
+++ showItemPageBE/src/main/java/com/talentpath/shamazin/showItemPage/controllers/ItemFamilyController.java
@@ -0,0 +1,15 @@
+package com.talentpath.shamazin.showItemPage.controllers;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/itemFamily")
+public class ItemFamilyController {
+    //todo: @Autowired ItemFamilyService when pull request gets approved
+
+}
