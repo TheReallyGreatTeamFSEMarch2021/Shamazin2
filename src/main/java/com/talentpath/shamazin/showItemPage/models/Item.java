b2bc2a15601dc85281307e3868eaa3b9d79b7ccb
Nathaniel Eveland <54957992+neveland98@users.noreply.github.com>
2021-03-12 16:04:13 -0600

    created property class, added to Item model and linked via foreign ke… (#38)
    * added getByFamilyId endpoint to item controller
    
    * added unit tests for service layer
    
    * wrote test for getByFamilyId golden path
    
    * created property class, added to Item model and linked via foreign key (one item to many properties)
    
    * deleted extra constructor as requested
    
    * changing merged unit tests to use the new Item constructor
    
    * added generatedvalue annotation to properties class

--- src/main/java/com/talentpath/shamazin/showItemPage/models/Item.java
+++ src/main/java/com/talentpath/shamazin/showItemPage/models/Item.java
@@ -25,6 +25,10 @@ public class Item {
     @OneToMany(mappedBy="item", cascade=CascadeType.ALL)
     private List<ProductPhoto> productPhotos;
 
+
+    @OneToMany(mappedBy="item", cascade=CascadeType.ALL)
+    private List<Property> properties;
+
     @JsonIgnore
     @ManyToMany(fetch=FetchType.LAZY)
     @JoinTable(
@@ -36,6 +40,7 @@ public class Item {
     @JsonIgnore
     @ManyToMany(mappedBy="relatedBoughtItems")
     private Set<Item> relatedItems;
+
 
     @NotBlank
     private String name;
@@ -115,13 +120,22 @@ public class Item {
 
     public Item() { }
 
-    public Item(@NotBlank ItemFamily itemFamily, List<ProductPhoto> productPhotos, @NotBlank String name, @NotBlank Double price, @NotBlank Integer stockRemaining, @NotBlank Boolean isPrimeEligible) {
+    public Item(@NotBlank ItemFamily itemFamily, List<ProductPhoto> productPhotos, List<Property> properties, @NotBlank String name, @NotBlank Double price, @NotBlank Integer stockRemaining, @NotBlank Boolean isPrimeEligible) {
         this.itemFamily = itemFamily;
         this.productPhotos = productPhotos;
+        this.properties = properties;
         this.name = name;
         this.price = price;
         this.stockRemaining = stockRemaining;
         this.isPrimeEligible = isPrimeEligible;
+    }
+
+    public List<Property> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(List<Property> properties) {
+        this.properties = properties;
     }
 
     @Override
@@ -129,11 +143,11 @@ public class Item {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Item item = (Item) o;
-        return itemFamily.equals(item.itemFamily) && Objects.equals(productPhotos, item.productPhotos) && name.equals(item.name) && price.equals(item.price) && stockRemaining.equals(item.stockRemaining) && isPrimeEligible.equals(item.isPrimeEligible);
+        return itemFamily.equals(item.itemFamily) && Objects.equals(properties,item.properties) && Objects.equals(productPhotos, item.productPhotos) && name.equals(item.name) && price.equals(item.price) && stockRemaining.equals(item.stockRemaining) && isPrimeEligible.equals(item.isPrimeEligible);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(itemFamily, productPhotos, name, price, stockRemaining, isPrimeEligible);
+        return Objects.hash(itemFamily, productPhotos, name, price, stockRemaining, isPrimeEligible, properties);
     }
 }
--- src/test/java/com/talentpath/shamazin/showItemPage/services/ItemServiceTest.java
+++ src/test/java/com/talentpath/shamazin/showItemPage/services/ItemServiceTest.java
@@ -78,8 +78,8 @@ class ItemServiceTest {
         ItemFamily doll = new ItemFamily("Doll XPS 13",null,null,null,"Doll");
         ItemFamily shamazinBasics = new ItemFamily("ShamazinBasics T-Shirt",null,null,null,"ShamazinBasics");
 
-        Item dollxps13 = new Item(doll,null,"Doll XPS 13 7450",1300D,50,true);
-        Item shamazinBasicsRedShirt = new Item(shamazinBasics,null,"Shamazin Basics T-Shirt (Red)",15D,200,true);
+        Item dollxps13 = new Item(doll,null,null,"Doll XPS 13 7450",1300D,50,true);
+        Item shamazinBasicsRedShirt = new Item(shamazinBasics,null,null,"Shamazin Basics T-Shirt (Red)",15D,200,true);
 
         itemFamilyRepo.saveAndFlush(doll);
         itemFamilyRepo.saveAndFlush(shamazinBasics);
@@ -98,7 +98,7 @@ class ItemServiceTest {
     void testAddItem() {
         ItemFamily hyperY = new ItemFamily("HyperY Cloud IX",null,null,null,"HyperY");
         itemFamilyRepo.saveAndFlush(hyperY);
-        Item hyperYCloudIX = new Item(hyperY,null,"HyperY Cloud IX (Red)",100D,100,true);
+        Item hyperYCloudIX = new Item(hyperY,null,null,"HyperY Cloud IX (Red)",100D,100,true);
         itemServe.addItem(hyperYCloudIX);
 
         Item testItem = itemRepo.findById(1).isPresent()?itemRepo.findById(1).get():null;
@@ -109,7 +109,7 @@ class ItemServiceTest {
     void deleteItem() {
         ItemFamily hyperY = new ItemFamily("HyperY Cloud IX",null,null,null,"HyperY");
         itemFamilyRepo.saveAndFlush(hyperY);
-        Item hyperYCloudIX = new Item(hyperY,null,"HyperY Cloud IX (Red)",100D,100,true);
+        Item hyperYCloudIX = new Item(hyperY,null,null,"HyperY Cloud IX (Red)",100D,100,true);
         itemServe.addItem(hyperYCloudIX);
 
         itemServe.deleteItem(1);
@@ -124,7 +124,7 @@ class ItemServiceTest {
     void getItem() {
         ItemFamily hyperY = new ItemFamily("HyperY Cloud IX",null,null,null,"HyperY");
         itemFamilyRepo.saveAndFlush(hyperY);
-        Item hyperYCloudIX = new Item(hyperY,null,"HyperY Cloud IX (Red)",100D,100,true);
+        Item hyperYCloudIX = new Item(hyperY,null,null,"HyperY Cloud IX (Red)",100D,100,true);
         itemServe.addItem(hyperYCloudIX);
 
         try {
@@ -141,7 +141,7 @@ class ItemServiceTest {
     void editItem() {
         ItemFamily hyperY = new ItemFamily("HyperY Cloud IX",null,null,null,"HyperY");
         itemFamilyRepo.saveAndFlush(hyperY);
-        Item hyperYCloudIX = new Item(hyperY,null,"HyperY Cloud IX (Red)",100D,100,true);
+        Item hyperYCloudIX = new Item(hyperY,null,null,"HyperY Cloud IX (Red)",100D,100,true);
         itemServe.addItem(hyperYCloudIX);
 
         hyperYCloudIX.setPrice(120D);
@@ -155,5 +155,27 @@ class ItemServiceTest {
         } catch (NoSuchItemException e) {
             fail("Exception caught during golden path test: " + e.getMessage());
         }
+    }
+
+    @Test
+    @Transactional
+    void getByFamilyId() {
+        ItemFamily doll = new ItemFamily("Doll XPS 13",null,null,null,"Doll");
+        ItemFamily shamazinBasics = new ItemFamily("ShamazinBasics T-Shirt",null,null,null,"ShamazinBasics");
+
+        Item dollxps13 = new Item(doll,null,null,"Doll XPS 13 7450",1300D,50,true);
+        Item shamazinBasicsRedShirt = new Item(shamazinBasics,null,null,"Shamazin Basics T-Shirt (Red)",15D,200,true);
+
+        itemFamilyRepo.saveAndFlush(doll);
+        itemFamilyRepo.saveAndFlush(shamazinBasics);
+
+        itemRepo.saveAndFlush(dollxps13);
+        itemRepo.saveAndFlush(shamazinBasicsRedShirt);
+
+        List<Item> dollList = itemServe.findByFamilyId(1);
+        List<Item> shamazinList = itemServe.findByFamilyId(2);
+
+        assertEquals(dollxps13,dollList.get(0));
+        assertEquals(shamazinBasicsRedShirt,shamazinList.get(0));
     }
 }
