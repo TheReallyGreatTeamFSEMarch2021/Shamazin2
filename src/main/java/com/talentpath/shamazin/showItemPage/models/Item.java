46d4f95c4c68cc8dadca9cae19b2428a97d7e860
neveland98 <54957992+neveland98@users.noreply.github.com>
2021-03-12 15:07:47 -0600

    created property class, added to Item model and linked via foreign key (one item to many properties)

--- src/main/java/com/talentpath/shamazin/showItemPage/models/Item.java
+++ src/main/java/com/talentpath/shamazin/showItemPage/models/Item.java
@@ -24,6 +24,9 @@ public class Item {
     @OneToMany(mappedBy="item", cascade=CascadeType.ALL)
     private List<ProductPhoto> productPhotos;
 
+    @OneToMany(mappedBy="item", cascade=CascadeType.ALL)
+    private List<Property> properties;
+
     @NotBlank
     private String name;
 
@@ -105,16 +108,34 @@ public class Item {
         this.isPrimeEligible = isPrimeEligible;
     }
 
+    public Item(@NotBlank ItemFamily itemFamily, List<ProductPhoto> productPhotos, List<Property> properties, @NotBlank String name, @NotBlank Double price, @NotBlank Integer stockRemaining, @NotBlank Boolean isPrimeEligible) {
+        this.itemFamily = itemFamily;
+        this.productPhotos = productPhotos;
+        this.properties = properties;
+        this.name = name;
+        this.price = price;
+        this.stockRemaining = stockRemaining;
+        this.isPrimeEligible = isPrimeEligible;
+    }
+
+    public List<Property> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(List<Property> properties) {
+        this.properties = properties;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Item item = (Item) o;
-        return itemFamily.equals(item.itemFamily) && Objects.equals(productPhotos, item.productPhotos) && name.equals(item.name) && price.equals(item.price) && stockRemaining.equals(item.stockRemaining) && isPrimeEligible.equals(item.isPrimeEligible);
+        return itemFamily.equals(item.itemFamily) && Objects.equals(properties,item.properties) && Objects.equals(productPhotos, item.productPhotos) && name.equals(item.name) && price.equals(item.price) && stockRemaining.equals(item.stockRemaining) && isPrimeEligible.equals(item.isPrimeEligible);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(itemFamily, productPhotos, name, price, stockRemaining, isPrimeEligible);
+        return Objects.hash(itemFamily, productPhotos, name, price, stockRemaining, isPrimeEligible, properties);
     }
 }
