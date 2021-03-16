b642ee3b55082b85a5ab53a7831900fcc31d2417
jlavendtt <68912686+jlavendtt@users.noreply.github.com>
2021-03-16 13:25:02 -0400

    Added info controller (#47)
    Co-authored-by: Jesse Lavendt <jesse@opal.dev>

---
+++ src/main/java/com/talentpath/shamazin/showItemPage/controllers/InfoController.java
@@ -0,0 +1,40 @@
+package com.talentpath.shamazin.showItemPage.controllers;
+
+import com.talentpath.shamazin.showItemPage.exceptions.NoSuchInfoException;
+import com.talentpath.shamazin.showItemPage.exceptions.NoSuchItemException;
+import com.talentpath.shamazin.showItemPage.exceptions.NullArgumentException;
+import com.talentpath.shamazin.showItemPage.models.Info;
+import com.talentpath.shamazin.showItemPage.models.Item;
+import com.talentpath.shamazin.showItemPage.services.InfoService;
+import com.talentpath.shamazin.showItemPage.services.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/info")
+public class InfoController {
+
+    @Autowired
+    InfoService service;
+
+    @GetMapping("/getAll")
+    public List<Info> getAllItems() {
+        return service.getAllInfo();
+    }
+
+    @GetMapping("/get/{id}")
+    public Info getInfoById(@PathVariable Integer id) throws  NoSuchInfoException {
+        return service.getInfo(id);
+    }
+
+    @GetMapping("/getByItemId/{ItemId}")
+    public List<Info> getInfosByItemId(@PathVariable Integer itemId) {
+
+        return service.getAllByItem(itemId);
+    }
+
+
+}
