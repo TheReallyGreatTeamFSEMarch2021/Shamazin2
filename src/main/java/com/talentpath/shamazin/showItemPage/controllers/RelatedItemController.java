42911f9dcc15f41165c534b955add77ee8312193
Tim <timbo.luu@gmail.com>
2021-03-16 17:54:10 -0400

    delete unnecessary import

--- src/main/java/com/talentpath/shamazin/showItemPage/controllers/RelatedItemController.java
+++ src/main/java/com/talentpath/shamazin/showItemPage/controllers/RelatedItemController.java
@@ -1,7 +1,6 @@
 package com.talentpath.shamazin.showItemPage.controllers;
 
 import com.talentpath.shamazin.showItemPage.models.Item;
-import com.talentpath.shamazin.showItemPage.services.ItemService;
 import com.talentpath.shamazin.showItemPage.services.RelatedBoughtItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
