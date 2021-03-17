5f603c633d44fe010623d9923ab534df95d66887
timluu <73354960+timluu@users.noreply.github.com>
2021-03-17 16:07:31 -0400

    add additional CRUD functionality for related items component (#49)
    * add delete functionality for related items component
    
    * delete unnecessary import
    
    * add paginated get route
    
    * add delete functionality for related items component
    
    * delete unnecessary import
    
    * add paginated get route

--- src/main/java/com/talentpath/shamazin/showItemPage/controllers/RelatedItemController.java
+++ src/main/java/com/talentpath/shamazin/showItemPage/controllers/RelatedItemController.java
@@ -3,16 +3,13 @@ package com.talentpath.shamazin.showItemPage.controllers;
 import com.talentpath.shamazin.showItemPage.models.Item;
 import com.talentpath.shamazin.showItemPage.services.RelatedBoughtItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RequestMapping("/api/related")
 @RestController
-public class RelatedItemsController {
+public class RelatedItemController {
 
     @Autowired
     RelatedBoughtItemService relatedBoughtItemService;
@@ -22,4 +19,13 @@ public class RelatedItemsController {
         return relatedBoughtItemService.getAllRelatedBoughtItems(itemId);
     }
 
+    @GetMapping("/bought/{itemId}/{page}")
+    public List<Item> getRelatedBoughtItemsByPage(@PathVariable Integer itemId, @PathVariable Integer page) {
+        return relatedBoughtItemService.getAllRelatedBoughtItemsByPage(itemId, page);
+    }
+
+    @DeleteMapping("/bought/{itemId}")
+    public void deleteRelatedBoughtItems(@PathVariable Integer itemId) {
+        relatedBoughtItemService.deleteAllRelatedBoughtItems(itemId);
+    }
 }
--- src/main/java/com/talentpath/shamazin/showItemPage/services/RelatedBoughtItemService.java
+++ src/main/java/com/talentpath/shamazin/showItemPage/services/RelatedBoughtItemService.java
@@ -5,9 +5,7 @@ import com.talentpath.shamazin.showItemPage.models.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class RelatedBoughtItemService {
@@ -26,6 +24,40 @@ public class RelatedBoughtItemService {
         relatedBoughtItems.addAll(item.getRelatedItems());
 
         return relatedBoughtItems;
+    }
+
+    public List<Item> getAllRelatedBoughtItemsByPage(Integer itemId, Integer page) {
+        List<Item> relatedBoughtItems = new ArrayList<>(getAllRelatedBoughtItems(itemId));
+        relatedBoughtItems.sort(Comparator.comparingInt(Item::getId));
+        Integer total = relatedBoughtItems.size();
+
+        if (total <= 7) {
+            return relatedBoughtItems;
+        }
+
+        Integer start = (page - 1) * 7;
+        Integer end = start + 7;
+
+        return end > total ? relatedBoughtItems.subList(start, total) : relatedBoughtItems.subList(start, end);
+    }
+
+    public void deleteAllRelatedBoughtItems(Integer itemId) {
+        Optional<Item> boughtItem = itemRepo.findById(itemId);
+
+        if (boughtItem.isPresent()) {
+            Item item = boughtItem.get();
+            Set<Item> relatedItems = item.getRelatedItems();
+
+            for (Item relatedItem : relatedItems) {
+                relatedItem.getRelatedBoughtItems().remove(item);
+            }
+
+            Set<Item> relatedBoughtItems = item.getRelatedBoughtItems();
+            relatedBoughtItems.removeAll(relatedBoughtItems);
+
+            itemRepo.flush();
+        }
+
     }
 
 }
