1840838ef98d3e8f6102e8413a5aa74b9e91b26d
jaitken <jaitken97@gmail.com>
2021-03-22 11:18:45 -0400

    added getFamilyId path to items

--- src/main/java/com/talentpath/shamazin/showItemPage/services/ItemService.java
+++ src/main/java/com/talentpath/shamazin/showItemPage/services/ItemService.java
@@ -55,4 +55,8 @@ public class ItemService {
     public List<Item> findByFamilyId(Integer familyId) {
         return itemDao.findAllByItemFamilyId(familyId);
     }
+
+    public Integer getFamilyId(Integer itemId) {
+        return itemDao.getFamilyId(itemId);
+    }
 }
